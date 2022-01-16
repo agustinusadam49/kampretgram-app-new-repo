a15642ab4f8193f2913cbe4d6406dfb096de39e3
Agustinus Adam <agustinusadamw@gmail.com>
2022-01-17 01:39:21 +0700

    Revert back register form requestOptions headers

--- client/src/components/RegisterForm.js
+++ client/src/components/RegisterForm.js
@@ -48,7 +48,7 @@ function RegisterForm({ setAuth }) {
 
         const requestOptions = {
             method: "POST",
-            headers: { "Content-Type": "application/json", "dataType": "json" },
+            headers: { "Content-Type": "application/json" },
             body: JSON.stringify(body)
         }
 

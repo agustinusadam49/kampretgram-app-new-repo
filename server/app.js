39001bc9763d3ccce254ce106160848c8f595f6a
Agustinus Adam <agustinusadamw@gmail.com>
2022-01-16 23:45:06 +0700

    Fix app.js file in server

--- server/app.js
+++ server/app.js
@@ -7,14 +7,14 @@ const express = require("express");
 const morgan = require("morgan");
 const app = express();
 const cors = require("cors");
-const http = require("http");const server = http.createServer(app);
+const http = require("http");
+const server = http.createServer(app);
 const io = require("socket.io")(server);
-
 const PORT = process.env.PORT || 5000;
 const indexRouter = require("./routes");
 const errorHandlers = require("./middlewares/errorHandlers");
 
-app.use(cors({origin: "https://kampretgram.netlify.app", credentials: true }));
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(morgan("dev"))

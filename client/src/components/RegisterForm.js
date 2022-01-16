1398d1deb273b7d923f848c4a34ed091aeffe1de
Agustinus Adam <agustinusadamw@gmail.com>
2022-01-17 01:34:55 +0700

    Fix dataType json in fetch requestOption for register api

--- client/src/components/RegisterForm.js
+++ client/src/components/RegisterForm.js
@@ -48,7 +48,7 @@ function RegisterForm({ setAuth }) {
 
         const requestOptions = {
             method: "POST",
-            headers: { "Content-Type": "application/json" },
+            headers: { "Content-Type": "application/json", "dataType": "json" },
             body: JSON.stringify(body)
         }
 
